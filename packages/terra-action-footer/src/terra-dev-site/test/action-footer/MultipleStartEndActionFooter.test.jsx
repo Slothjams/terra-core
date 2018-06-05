221394d2c664b9d7abdd6aa41da5cf2f5a66c0f0
Brett Jankord <brett.jankord@cerner.com>
2018-06-05 16:46:26 -0500

    Update tests

--- packages/terra-action-footer/src/terra-dev-site/test/action-footer/MultipleStartEndActionFooter.test.jsx
+++ packages/terra-action-footer/src/terra-dev-site/test/action-footer/MultipleStartEndActionFooter.test.jsx
@@ -3,19 +3,21 @@ import Button from 'terra-button';
 import ActionFooter from '../../../ActionFooter';
 
 export default () => (
-  <ActionFooter
-    start={
-      <div>
-        FakeLink Start Action 1
-        <Button text="Start Action 2" />
-      </div>
-    }
-    end={
-      <div>
-        <Button text="End Action 1" />
-        FakeLink End Action 2
-        <Button text="End Action 3" />
-      </div>
-    }
-  />
+  <div style={{ padding: '20px' }}>
+    <ActionFooter
+      start={
+        <div>
+          FakeLink Start Action 1
+          <Button text="Start Action 2" />
+        </div>
+      }
+      end={
+        <div>
+          <Button text="End Action 1" />
+          FakeLink End Action 2
+          <Button text="End Action 3" />
+        </div>
+      }
+    />
+  </div>
 );
